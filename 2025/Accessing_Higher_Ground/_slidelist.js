d5d0dbcfe20cf63395e49e3dd52a3955f605a755
Doug Schepers <doug@schepers.cc>
2025-11-18 10:47:44 -0700

    Adapting A11yTO talk to include integrated demos with scrollytelling

---
+++ 2025/Accessing_Higher_Ground/_slidelist.js
@@ -0,0 +1,38 @@
+var slidelist = [
+  'intro.svg',
+  'wcag-dataviz-iceberg.svg',
+  'traditional-chart-a11y.svg',
+
+  'alt-text-limitations.svg',
+  'alt-text-hard.svg',
+
+  'data-table-benefits.svg',
+  'data-table-weakness.svg',
+
+  'picture-superiority-effect.svg',
+  'storytelling-effectiveness.svg',
+
+  'personalization.svg',
+  'clt-and-disabilities.svg',
+  'part-to-whole-to-part.svg',
+
+  'chart-a11y-basics.svg',
+  'chartability.svg',
+  'demo-paracharts.svg',
+  'visual-best-practices.html',
+  'colors-and-patterns.html',
+  'multimodal.html',
+
+  'ai-descriptions.html',
+  'chart-exploration.html',
+  
+  'underlay.svg',
+  'llm-summaries.html',
+  'ml-nlg-approach.svg',
+  // 'genai.svg',
+  // 'project-status.svg',
+  'ancillary-projects.html',
+  'thanks.svg',
+  'paracharts-architecture.svg',
+  'salience.svg',
+ ];
